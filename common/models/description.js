cc8a2966efc76927064949f85942902caf3faf19
aldgagnon <aldgagnon@gmail.com>
2016-01-27 23:04:41 -0500

    Removal of /Random remote method, clean up of console.logs

--- common/models/description.js
+++ common/models/description.js
@@ -7,87 +7,44 @@ module.exports = function(Description) {
   Description.disableRemoteMethod("upsert", true);
   Description.disableRemoteMethod("updateAll", true);
   Description.disableRemoteMethod("updateAttributes", false);
-  //Description.disableRemoteMethod("findById", false);
   Description.disableRemoteMethod("findOne", true);
+  Description.disableRemoteMethod("findById", true);
   Description.disableRemoteMethod("deleteById", true);
   Description.disableRemoteMethod("confirm", true);
-  Description.disableRemoteMethod("count", false);
+  Description.disableRemoteMethod("count", true);
   Description.disableRemoteMethod("exists", true);
   Description.disableRemoteMethod("resetPassword", true);
   Description.disableRemoteMethod("createChangeStream", true);
 
-  Description.remoteMethod(
-    'Random',
-    {
-      http: {path: '/Random', verb: 'get'},
-      returns: { arg: 'Results', type: 'string'}
-    });
 
-    Description.afterRemote('find', function(ctx,user,next) {
-      var des = ctx.result;
-      var tes = JSON.parse('{ "name" : "andrew" }');
-      console.log(tes);
-      console.log(tes.name);
-
-      console.log('after remote');
-      console.log(des[0].name);
-
-      // console.log(description);
-      // ctx.result = '{ "name" : ' + description + '}';
-
-      next();
-
-    });
-
-    Description.beforeRemote('find', function(ctx,user,next) {
-      console.log('find beforeRemote');
-
-      Description.count(null, function(err,res) {
-        if(err) {
-          console.log(err);
-          cb(err);
-        }
-        else {
-
-          var randomValue = random.integer(1,res);
-          console.log(res);
-          ctx.args.filter = '{ "where" : { "id": ' + res + '}}';
-          console.log(ctx.args.filter);
-
-
-          next();
-        }
-      });
-    });
-
-
-    Description.Random = function(cb) {
-      Description.count(null, function(err,res) {
-        if(err) {
-          console.log(err);
-          cb(err);
-        }
-        else {
-
-          var randomValue = random.integer(1,res);
-
-
-
-
-          Description.findById(randomValue, function(err,res) {
-            if(err) {
-              console.log(err);
-              cb(err);
-            }
-            else {
-              var DescriptiveRubberDucky = res.name + ' Rubber Duck';
-              cb(null,DescriptiveRubberDucky);
-            }
-          });
-
-        }
-      });
+  Description.afterRemote('find', function(ctx,user,next) {
+    if(ctx) {
+      ctx.result = ctx.result[0].name + ' Rubber Duck';
     }
 
+    next();
 
-  };
+  });
+
+  Description.beforeRemote('find', function(ctx,user,next) {
+
+    Description.count(null, function(err,res) {
+      if(err) {
+        console.log(err);
+        cb(err);
+      }
+      else {
+
+        var randomValue = random.integer(1,res);
+        console.log(res);
+        ctx.args.filter = '{ "where" : { "id": ' + randomValue + '}}';
+        console.log(ctx.args.filter);
+
+
+        next();
+      }
+    });
+  });
+
+
+};
